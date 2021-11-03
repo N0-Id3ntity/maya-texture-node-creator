c459a460acba546a2ce7cbf50e99ffee0bc60b04
N0-Id3ntity <santucci.francesca95@gmail.com>
2021-11-03 22:43:38 +0100

    Create file dialog to select the texture folder

--- material_node_converter.py
+++ material_node_converter.py
@@ -2,9 +2,17 @@ import os
 import maya.cmds as cmds
 
 class TNC_Window(object):
-    def close_window(self, *_):
+    def close_window_button_clicked(self, *_):
         cmds.deleteUI(self.window, window=True)
         
+    def browser_button_clicked(self, *_):
+        print("Browser button clicked", os.listdir(self.startingDir))
+        multipleFilters = "Image (*.png *.jpeg *.jpg *.gif *.tif *.iff);;"
+        cmds.fileDialog2(startingDirectory=self.startingDir, fileFilter=multipleFilters, okCaption="Load")
+
+    def convert_button_clicked(self, *_):
+       print("Continue conversion process")
+   
     def __init__(self):
         self.window = "Texture nodes creator"
         self.title = "Texture nodes creator"
@@ -13,14 +21,21 @@ class TNC_Window(object):
             cmds.deleteUI(self.window, window=True)
 
         self.window = cmds.window(self.window, title=self.title)
-        cmds.columnLayout(adjustableColumn=True)
+        cmds.columnLayout("root", adjustableColumn=True)
 
         
         projectDirectory = cmds.workspace(q=True, rd=True)
-        startingDir = projectDirectory if projectDirectory else os.environ["MAYA_APP_DIR"]
-        cmds.fileDialog2(caption="Select the texture's folder...", startingDirectory=startingDir, dialogStyle=2, fileFilter="*.png;;*.jpg;;*.jpeg;;*.sgi;;*.iff;;")
+        self.startingDir = projectDirectory if projectDirectory else os.environ["MAYA_APP_DIR"]
+
+        cmds.rowLayout("directoryRow", numberOfColumns=2, adjustableColumn=True, parent="root")
+        dirTextBox = cmds.textField(text=self.startingDir)
+        browser_button = cmds.button("Browser", command=self.browser_button_clicked)
         
-        button = cmds.button("Close", command=self.close_window)
+        cmds.separator(height=3, style="in", parent="root")
+        
+        cmds.rowLayout("buttonsRow", numberOfColumns=2, parent="root", columnAlign=(12, "right"))
+        convert_button = cmds.button("Convert", command=self.convert_button_clicked, parent="buttonsRow")
+        close_button = cmds.button("Cancel", command=self.close_window_button_clicked, parent="buttonsRow")
 
         cmds.showWindow()
 
