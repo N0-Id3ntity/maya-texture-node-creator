978544662a264228b2313b1b6c37f5e71999d546
N0-Id3ntity <santucci.francesca95@gmail.com>
2021-11-03 23:57:45 +0100

    Load correct path in text filed.

--- material_node_converter.py
+++ material_node_converter.py
@@ -1,18 +1,30 @@
+import re
 import os
+from pathlib import Path
 import maya.cmds as cmds
 
 class TNC_Window(object):
     def close_window_button_clicked(self, *_):
         cmds.deleteUI(self.window, window=True)
-        
+
+    def load_button_clicked(self, *_):
+        print("AOOOOOOO")
+
     def browser_button_clicked(self, *_):
         print("Browser button clicked", os.listdir(self.startingDir))
         multipleFilters = "Image (*.png *.jpeg *.jpg *.gif *.tif *.iff);;"
-        cmds.fileDialog2(startingDirectory=self.startingDir, fileFilter=multipleFilters, okCaption="Load")
+        file_dialog = cmds.fileDialog2(startingDirectory=self.startingDir, fileFilter=multipleFilters, fileMode=4, okCaption="Load", optionsUICommit=self.load_button_clicked)
+
+        self.texture_path = file_dialog
+
+        print("TEXTURE_DIR_PATH: ", self.texture_path[0])
+        if self.texture_path:
+            self.texture_path=Path(self.texture_path[0]).parent
+            cmds.textField(self.dirTextBox, text=self.texture_path, edit=True)
 
     def convert_button_clicked(self, *_):
        print("Continue conversion process")
-   
+
     def __init__(self):
         self.window = "Texture nodes creator"
         self.title = "Texture nodes creator"
@@ -23,12 +35,11 @@ class TNC_Window(object):
         self.window = cmds.window(self.window, title=self.title)
         cmds.columnLayout("root", adjustableColumn=True)
 
-        
         projectDirectory = cmds.workspace(q=True, rd=True)
         self.startingDir = projectDirectory if projectDirectory else os.environ["MAYA_APP_DIR"]
 
         cmds.rowLayout("directoryRow", numberOfColumns=2, adjustableColumn=True, parent="root")
-        dirTextBox = cmds.textField(text=self.startingDir)
+        self.dirTextBox = cmds.textField(text=self.startingDir)
         browser_button = cmds.button("Browser", command=self.browser_button_clicked)
         
         cmds.separator(height=3, style="in", parent="root")
